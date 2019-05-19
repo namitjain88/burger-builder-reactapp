a8de4997f90b6590dcccc1f27fcdd61ca79444ad
Namit Jain <namit.jain88@gmail.com>
2019-05-19 10:06:34 -0500

    Applied following optimization: 1. Moved the utility.js to shared folder under src. 2. Used updateObject in Auth and ContactData containers. 3. Moved checkValidity to utility.js

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import classes from './Auth.css';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
 
@@ -44,47 +45,14 @@ class Auth extends Component {
     }
 
     inputChangeHandler = (event, inputName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [inputName]: {
-                ...this.state.controls[inputName],
+        const updatedControls = updateObject(this.state.controls, {
+            [inputName]: updateObject(this.state.controls[inputName], {
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[inputName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[inputName].validation),
                 touched: true
-            }
-        };
+            })
+        });
         this.setState({ controls: updatedControls });
-    }
-
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
     }
 
     submitHandler = (event) => {
